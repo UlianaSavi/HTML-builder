1963bd54564b430cdc41c7f36fd1b6dacc6af98e
UlianaSavi <uljasha.sav@gmail.com>
2022-10-31 20:31:42 +0600

    feat: add ability to writing console input to a file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,31 @@
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
+const readline = require('node:readline');
+const { stdin: input, stdout: output} = require('node:process');
 
-fs.writeFile("text.txt", "Текст", function(err){
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Файл создан");
-    }
+const wr = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+wr.write('');
+
+const rl = readline.createInterface({ input, output });
+
+function ask(question) {
+    rl.question(question, (answer) => {
+        if (answer === 'exit') {
+            console.log('Bye my friend!');
+            wr.end();
+            rl.close();
+            return;
+        }
+
+        wr.write(`${answer}`);
+        ask('What do you think of Node.js? ');
+    });
+}
+
+ask('What do you think of Node.js? ');
+
+rl.on('SIGINT', () => {
+    console.log('\nBye my friend!');
+    rl.close();
 });

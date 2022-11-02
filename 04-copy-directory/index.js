9408e1b705c120ff537f687b6ed40b92d20ee7f8
UlianaSavi <uljasha.sav@gmail.com>
2022-11-02 15:58:19 +0600

    feat: add ability to copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+((async () => {
+    await fs.mkdir('04-copy-directory/files-copy', { recursive: true });
+    const files = await fs.readdir('04-copy-directory/files');
+    console.log(files);
+
+    await Promise.all(files.map(async (file) => {
+        await fs.copyFile(path.join(`${__dirname}/files`, file), path.join(`${__dirname}/files-copy`, file));
+    }));
+})())
